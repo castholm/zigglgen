7d3b1504231ece877cd022187c08a80e360f6990
Carl Åstholm <carl@astholm.se>
2024-10-27 15:39:20 +0100

    Add `run` and `test` steps to facilitate sanity checking

--- build.zig
+++ build.zig
@@ -11,6 +11,30 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
     b.installArtifact(zigglgen_exe);
+
+    const run_zigglgen = b.addRunArtifact(zigglgen_exe);
+    run_zigglgen.addArgs(b.args orelse &.{});
+    run_zigglgen.step.dependOn(b.getInstallStep());
+
+    const run_step = b.step("run", "Run zigglgen");
+    run_step.dependOn(&run_zigglgen.step);
+
+    const zigglgen_tests = b.addTest(.{
+        .root_source_file = generate_everything: {
+            const r = b.addRunArtifact(zigglgen_exe);
+            r.addArgs(&.{ "gl-4.6-core", "ZIGGLGEN_everything" });
+            const output = r.captureStdOut();
+            r.captured_stdout.?.basename = "gl.zig";
+            break :generate_everything output;
+        },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const run_tests = b.addRunArtifact(zigglgen_tests);
+
+    const test_step = b.step("test", "Sanity check zigglgen output");
+    test_step.dependOn(&run_tests.step);
 }
 
 pub const GeneratorOptions = @import("GeneratorOptions.zig");
@@ -31,7 +55,9 @@ pub fn generateBindingsSourceFile(b: *std.Build, options: GeneratorOptions) std.
         if (options.profile != null) "-" else "",
         if (options.profile) |profile| @tagName(profile) else "",
     }));
-    for (options.extensions) |extension| run_zigglgen.addArg(@tagName(extension));
+    for (options.extensions) |extension| {
+        run_zigglgen.addArg(@tagName(extension));
+    }
     const output = run_zigglgen.captureStdOut();
     run_zigglgen.captured_stdout.?.basename = "gl.zig";
     return output;
